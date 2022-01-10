29db76c80c5622a6ce7b2bcd91c48c4a37b461c1
成龙生 <1536463948@qq.com>
2022-01-10 18:05:37 +0800

    队列（只能用一次）

---
+++ DataStructures/src/pers/cls/arrayqueue/ArrayQueue.java
@@ -0,0 +1,69 @@
+package pers.cls.arrayqueue;
+
+public class ArrayQueue {
+
+    private final int maxSize;
+    private int head;
+    private int tail;
+    private final int[] arr;
+
+    public ArrayQueue(int maxSize) {
+        this.maxSize = maxSize;
+        head = -1;
+        tail = -1;
+        arr = new int[maxSize];
+    }
+
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    public boolean isFull() {
+        return tail == maxSize - 1;
+    }
+
+    public void addQueue(int value) {
+        if (isFull()) {
+            System.out.println("队列已满");
+        } else {
+            tail++;
+            arr[tail] = value;
+        }
+    }
+
+    public void getQueue() {
+        if (isEmpty()) {
+            System.out.println("队列为空");
+        } else {
+            head++;
+            System.out.println("获取的值：" + arr[head]);
+        }
+    }
+
+    public void showQueue() {
+        if (isEmpty()) {
+            System.out.println("队列为空");
+        } else {
+            System.out.print("当前队列：");
+            for (int i : arr) {
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void showHead() {
+        if (isEmpty()) {
+            System.out.println("队列为空");
+        } else {
+//            if (head == -1) {
+                System.out.println("当前队头：" + arr[head + 1]);
+//            } else {
+//                System.out.println("当前队头：" + arr[head]);
+//            }
+
+        }
+    }
+
+}
---
+++ DataStructures/src/pers/cls/arrayqueue/ArrayQueueDemo.java
@@ -0,0 +1,43 @@
+package pers.cls.arrayqueue;
+
+import java.util.Scanner;
+
+public class ArrayQueueDemo {
+
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(3);
+        char key;
+        Scanner scanner = new Scanner(System.in);
+
+        boolean loop = true;
+        while (loop) {
+            System.out.println("s（show）：显示队列");
+            System.out.println("e（exit）：退出程序");
+            System.out.println("a（add）：添加数据到队列");
+            System.out.println("g（get）：从队列获取数据");
+            System.out.println("h（head）：显示队头数据");
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'e':
+                    loop = false;
+                    break;
+                case 'a':
+                    System.out.print("请输入要添加的号码数：");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    queue.getQueue();
+                    break;
+                case 'h':
+                    queue.showHead();
+                    break;
+            }
+        }
+
+    }
+
+}
